de4e01f976ee919fa6704d2014ea3d076b7cddaf
koreahong <koreahong2@naver.com>
2021-08-24 21:30:57 +0900

    F

--- googleAPI.py
+++ googleAPI.py
@@ -11,10 +11,10 @@ def orc_kor_eng(image_file):
     image = types.Image(content=content)
     response = client.text_detection(image=image)
     labels = response.text_annotations
-    res = ''
-    for  label in labels:
-        res += label.description + '\n'
-
+    res = labels[0].description
+    # for  label in labels:
+    #     res += label.description + '\n'
+    # print(labels)
     return res
 
 client = vision.ImageAnnotatorClient()
